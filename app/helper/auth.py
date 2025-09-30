8f367e8a336a22a584c17729f40714c570c980e9
LiaoAnn <1050123tom@gmail.com>
2025-09-30 07:50:18 +0000

    chore: remove useless feature

--- app/helper/auth.py
+++
@@ -1,7 +0,0 @@
-from core.config import SECRET_KEY
-from fastapi import Header, HTTPException, status
-
-
-def verify_token(token: str = Header(...)):
-    if token != SECRET_KEY:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Token invalid")
--- app/router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.health.router import router as health_router
-
-router = APIRouter()
-
-router.include_router(health_router, tags=["health"], prefix="/health")
